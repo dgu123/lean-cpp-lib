7e629409cfdad217229e6962478ae96e4b5eb54d
opensource@alphanew.net <opensource@alphanew.net@3a0cca5d-a9e9-79d7-8999-1955a74f608b>
2011-03-16 23:55:24 +0000

    - introduced lean heap to STL allocator adapter class  -> allows for aligned allocators

---
+++ trunk/lean/header/lean/memory/heap_allocator.h
@@ -0,0 +1,180 @@
+/*****************************************************/
+/* lean Memory                  (c) Tobias Zirr 2011 */
+/*****************************************************/
+
+#ifndef LEAN_MEMORY_HEAP_ALLOCATOR
+#define LEAN_MEMORY_HEAP_ALLOCATOR
+
+#include "../lean.h"
+#include "../meta/strip.h"
+
+namespace lean
+{
+namespace memory
+{
+
+/// STL allocator heap adapter.
+template<class Element, class Heap, size_t Alignment = 1>
+class heap_allocator
+{
+public:
+	/// Heap adapted by this heap allocator.
+	typedef Heap heap_type;
+
+	/// Value type.
+	typedef typename strip_const<Element>::type value_type;
+
+	/// Pointer type.
+	typedef typename value_type* pointer;
+	/// Reference type.
+	typedef typename value_type& reference;
+	/// Pointer type.
+	typedef typename const value_type* const_pointer;
+	/// Reference type.
+	typedef typename const value_type& const_reference;
+
+	/// Size type.
+	typedef typename heap_type::size_type size_type;
+	/// Pointer difference type.
+	typedef ptrdiff_t difference_type;
+
+	/// Allows for the creation of differently-typed equivalent allocators.
+	template<class Other>
+	struct rebind
+	{
+		/// Equivalent allocator allocating elements of type Other.
+		typedef heap_allocator<Other, Heap, Alignment> other;
+	};
+	
+	/// Default constructor.
+	LEAN_INLINE heap_allocator() { }
+	/// Copy constructor.
+	template<class Other>
+	LEAN_INLINE heap_allocator(const heap_allocator<Other, Heap, Alignment> &right) { }
+	/// Assignment operator.
+	template<class Other>
+	LEAN_INLINE heap_allocator& operator=(const heap_allocator<Other, Heap, Alignment> &right) { return *this; }
+	
+	/// Allocates the given number of elements.
+	LEAN_INLINE pointer allocate(size_type count)
+	{
+		return heap_type::allocate<Alignment>(count * sizeof(value_type));
+	}
+	/// Allocates the given amount of memory.
+	LEAN_INLINE pointer allocate(size_type count, const void *)
+	{
+		return allocate(count);
+	}
+	/// Allocates the given amount of memory.
+	LEAN_INLINE void deallocate(pointer ptr, size_type)
+	{
+		heap_type::free<Alignment>(ptr);
+	}
+
+	/// Constructs a new element from the given value at the given pointer.
+	LEAN_INLINE void construct(pointer ptr, const value_type& value)
+	{
+		new(ptr) Element(value);
+	}
+	/// Constructs a new element from the given value at the given pointer.
+	template<class Other>
+	LEAN_INLINE void construct(pointer ptr, const Other& value)
+	{
+		new(ptr) Element(value);
+	}
+	/// Constructs a new element from the given value at the given pointer.
+	LEAN_INLINE void construct(pointer ptr, value_type&& value)
+	{
+		new(ptr) Element(std::move(value));
+	}
+	/// Constructs a new element from the given value at the given pointer.
+	template<class Other>
+	LEAN_INLINE void construct(pointer ptr, Other&& value)
+	{
+		new(ptr) Element(std::move(value));
+	}
+	/// Destructs an element at the given pointer.
+	LEAN_INLINE void destroy(pointer ptr)
+	{
+		ptr->~Element();
+	}
+
+	/// Gets the address of the given element.
+	LEAN_INLINE pointer address(reference value) const
+	{
+		return reinterpret_cast<pointer>( &reinterpret_cast<char&>(value) );
+	}
+	/// Gets the address of the given element.
+	LEAN_INLINE const_pointer address(const_reference value) const
+	{
+		return reinterpret_cast<const_pointer>( &reinterpret_cast<const char&>(value) );
+	}
+
+	/// Estimates the maximum number of elements that may be constructed.
+	LEAN_INLINE size_t max_size() const
+	{
+		size_t count = reinterpret_cast<size_t>(-1) / sizeof(Element);
+		return (0 < count) ? count : 1;
+	}
+};
+
+/// STL allocator heap adapter.
+template<class Heap, size_t Alignment = 1>
+class heap_allocator<void, Heap, Alignment>
+{
+public:
+	/// Heap adapted by this heap allocator.
+	typedef Heap heap_type;
+
+	/// Value type.
+	typedef void value_type;
+
+	/// Pointer type.
+	typedef typename value_type* pointer;
+	/// Pointer type.
+	typedef typename const value_type* const_pointer;
+
+	/// Size type.
+	typedef typename heap_type::size_type size_type;
+	/// Pointer difference type.
+	typedef ptrdiff_t difference_type;
+
+	/// Allows for the creation of differently-typed equivalent allocators.
+	template<class Other>
+	struct rebind
+	{
+		/// Equivalent allocator allocating elements of type Other.
+		typedef heap_allocator<Other, Heap, Alignment> other;
+	};
+	
+	/// Default constructor.
+	LEAN_INLINE heap_allocator() { }
+	/// Copy constructor.
+	template<class Other>
+	LEAN_INLINE heap_allocator(const heap_allocator<Other, Heap, Alignment> &right) { }
+	/// Assignment operator.
+	template<class Other>
+	LEAN_INLINE heap_allocator& operator=(const heap_allocator<Other, Heap, Alignment> &right) { return *this; }
+};
+
+/// Checks the given two allocators for equivalence.
+template<class Element, class Heap, size_t Alignment, class Other>
+LEAN_INLINE bool operator ==(const heap_allocator<Element, Heal, Alignment>&, const allocator<Other, Heap, Alignment>&)
+{
+	return true;
+}
+
+/// Checks the given two allocators for inequivalence.
+template<class Element, class Heap, size_t Alignment, class Other>
+LEAN_INLINE bool operator !=(const heap_allocator<Element, Heal, Alignment>&, const allocator<Other, Heap, Alignment>&)
+{
+	return false;
+}
+
+} // namespace
+
+using memory::heap_allocator;
+
+} // namespace
+
+#endif
---
+++ trunk/lean/header/lean/memory/memory.h
@@ -0,0 +1,14 @@
+/*****************************************************/
+/* lean Memory                  (c) Tobias Zirr 2011 */
+/*****************************************************/
+
+#ifndef LEAN_MEMORY
+#define LEAN_MEMORY
+
+#include "alignment.h"
+#include "crt_heap.h"
+#include "aligned.h"
+
+#include "heap_allocator.h"
+
+#endif

e3b84cd13deb7ab5934bbf61be367f4b563cd944
opensource <opensource@alphanew.net>
2011-03-16 23:21:03 +0000

    - introduced heap concept that deals with raw memory allocation as opposed to STL allocators working on objects  - introduced support for aligned memory allocation, inspired by The Cric Framework :-)

---
+++ lean/header/lean/memory/aligned.h
@@ -0,0 +1,53 @@
+/*****************************************************/
+/* lean Memory                  (c) Tobias Zirr 2011 */
+/*****************************************************/
+
+#ifndef LEAN_MEMORY_ALIGNED
+#define LEAN_MEMORY_ALIGNED
+
+#include "../lean.h"
+#include "alignment.h"
+#include "crt_heap.h"
+
+namespace lean
+{
+namespace memory
+{
+	/// Aligns derived classes according to the given alignment template argument.
+	template <size_t Alignment, class Heap = crt_heap>
+	class aligned : public stack_aligned<Alignment>
+	{
+	protected:
+		LEAN_INLINE aligned() { };
+		LEAN_INLINE ~aligned() throw() { };
+
+	public:
+		/// Allocates an aligned block of memory of the given size.
+		LEAN_INLINE void* operator new(size_t size)
+		{
+			return Heap::allocate<Alignment>(size);
+		}
+		/// Allocates an aligned block of memory of the given size.
+		LEAN_INLINE void* operator new[](size_t size)
+		{
+			return Heap::allocate<Alignment>(size);
+		}
+		/// Frees an aligned block of memory of the given size.
+		LEAN_INLINE void operator delete(void *memory)
+		{
+			return Heap::free<Alignment>(memory);
+		}
+		/// Allocates an aligned block of memory of the given size.
+		LEAN_INLINE void operator delete[](void *memory)
+		{
+			return Heap::free<Alignment>(memory);
+		}
+	};
+
+} // namespace
+
+using memory::aligned;
+
+} // namespace
+
+#endif

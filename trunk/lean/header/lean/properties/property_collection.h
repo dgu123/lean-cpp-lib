fe9fc4f624ccc5c44678a21dcaf556494ea031e8
opensource@alphanew.net <opensource@alphanew.net@3a0cca5d-a9e9-79d7-8999-1955a74f608b>
2011-03-21 23:51:26 +0000

    - property_collection prototype

--- trunk/lean/header/lean/properties/property_collection.h
+++ trunk/lean/header/lean/properties/property_collection.h
@@ -14,52 +14,73 @@ namespace lean
 namespace properties
 {
 
-template <class Class>
+template <class Class, class Description>
 class property_collection
 {
 private:
-	std::vector<property_description> m_properties;
+	std::vector<Description> m_properties;
 
 public:
+	/// Property description type.
+	typedef Description property_desc;
+
 	property_collection();
 	property_collection(const property_collection &inherit);
 
-	size_t add()
+	/// Adds a property created from the given property description.
+	LEAN_INLINE size_t add(const property_desc& propertyDesc)
 	{
 		size_t id = m_properties.size();
-		// TODO: add
+		m_properties.push_back(propertyDesc);
 		return id;
 	}
-	
-	size_t count() const
+	/// Gets the property description for the given ID, access violation on failure.
+	LEAN_INLINE const property_desc& desc(size_t id) const
+	{
+		return m_properties[id];
+	}
+	/// Gets the number of properties stored by this collection.
+	LEAN_INLINE size_t count() const
 	{
 		return m_properties.size();
 	}
 
+	/// Assigns the given value to the property identified by the given id, fails silently.
 	template <class Value>
-	void set(Class &object, size_t id, const Value &value) const
+	LEAN_INLINE void set(Class &object, size_t id, const Value &value)
 	{
 		set(object, id, &value, 1);
 	}
-
+	/// Assigns the given values to the property identified by the given id, fails silently.
 	template <class Value>
-	void set(Class &object, size_t id, const Value *values, size_t count) const
+	LEAN_INLINE void set(Class &object, size_t id, const Value *values, size_t count)
 	{
 		if (id < m_properties.size())
-			m_properties[id].setter(object, typeid(Value), values, count);
+		{
+			Description &desc = m_properties[id];
+			
+			if (desc.setter.getptr())
+				desc.setter(object, typeid(Value), values, count);
+		}
 	}
 
+	/// Reads a value from the property identified by the given id, fails silently.
 	template <class Value>
-	void get(const Class &object, size_t id, Value &value) const
+	LEAN_INLINE void get(const Class &object, size_t id, Value &value) const
 	{
 		get(object, id, &value, 1);
 	}
-
+	/// Reads values from the property identified by the given id, fails silently.
 	template <class Value>
-	void get(const Class &object, size_t id, Value *values, size_t count) const
+	LEAN_INLINE void get(const Class &object, size_t id, Value *values, size_t count) const
 	{
 		if (id < m_properties.size())
-			m_properties[id].getter(object, typeid(Value), values, count);
+		{
+			const Description &desc = m_properties[id];
+
+			if (desc.getter.getptr())
+				desc.getter(object, typeid(Value), values, count);
+		}
 	}
 
 };

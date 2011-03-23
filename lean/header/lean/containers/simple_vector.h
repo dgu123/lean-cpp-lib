0f875d2d5092d8070703799cb5990dc89d6f56f0
opensource <opensource@alphanew.net>
2011-03-23 00:14:04 +0000

    - simple_vector implementation continued

--- lean/header/lean/containers/simple_vector.h
+++ lean/header/lean/containers/simple_vector.h
@@ -28,6 +28,36 @@ private:
 
 	static const size_t s_max_size = reinterpret_cast<size_t>(-1) / sizeof(Element);
 
+	/// Default constructs an element at the given location.
+	LEAN_INLINE void default_construct(Element *dest)
+	{
+		m_allocator.construct(dest, Element());
+	}
+	/// Default constructs elements in the given range.
+	void default_construct(Element *dest, Element *destEnd)
+	{
+		Element *destr = dest;
+
+		try
+		{
+			for (; dest != destEnd; ++dest)
+				default_construct(dest);
+		}
+		catch(...)
+		{
+			destruct(destr, dest);
+			throw;
+		}
+	}
+	/// Moves the given source element to the given destination.
+	LEAN_INLINE void move_construct(Element *source, Element *dest)
+	{
+#ifndef LEAN0X_NO_RVALUE_REFERENCES
+		m_allocator.construct(dest, std::move(*source));
+#else
+		m_allocator.construct(dest, *source);
+#endif
+	}
 	/// Moves elements from the given source range to the given destination.
 	void move_construct(Element *source, Element *sourceEnd, Element *dest)
 	{
@@ -36,11 +66,7 @@ private:
 		try
 		{
 			for (; source != sourceEnd; ++dest, ++source)
-#ifndef LEAN0X_NO_RVALUE_REFERENCES
-				m_allocator.construct(dest, std::move(*source));
-#else
-				m_allocator.construct(dest, *source);
-#endif
+				move_construct(dest, source);
 		}
 		catch(...)
 		{
@@ -50,10 +76,15 @@ private:
 	}
 
 	/// Destructs the elements in the given range.
+	LEAN_INLINE void destruct(Element *destr)
+	{
+		m_allocator.destroy(*destr);
+	}
+	/// Destructs the elements in the given range.
 	void destruct(Element *destr, Element *destrEnd)
 	{
 		for (; destr != destrEnd; ++destr)
-			m_allocator.destroy(*destr);
+			destruct(destr);
 	}
 
 	/// Allocates space for the given number of elements.
@@ -68,7 +99,7 @@ private:
 			}
 			catch(...)
 			{
-				m_allocator.deallocate(newElements, capacity);
+				m_allocator.deallocate(newElements, newCapacity);
 				throw;
 			}
 
@@ -173,17 +204,23 @@ public:
 	/// Removes the last element from this vector.
 	LEAN_INLINE void pop_back()
 	{
-		// TODO: implement
+		if (m_count > 0)
+		{
+			--m_count;
+			destruct(m_elements + m_count);
+		}
+
+		// TODO: exception?
 	}
 
 	/// Clears all elements from this vector.
 	LEAN_INLINE void clear()
 	{
-		if (m_elements)
+		if (m_count > 0)
 		{
-			Element *end = m_elements + m_count;
+			Element *elementsOldEnd = m_elements + m_count;
 			m_count = 0;
-			destruct(m_elements, end);
+			destruct(m_elements, elementsOldEnd);
 		}
 	}
 
@@ -193,10 +230,64 @@ public:
 		if (newCapacity > m_capacity)
 			reallocate(newCapacity);
 	}
+	/// Resizes this vector, either appending empty elements to or removing elements from the back of this vector.
+	void resize(size_t newCount)
+	{
+		if (newCount > m_count)
+		{
+			if (newCount > m_capacity)
+				reallocate(capacity_hint(newCount));
+			
+			default_construct(m_elements + m_count, m_elements + newCount);
+			m_count = newCount;
+		}
+		// Handle newCount == m_count == 0 (m_elements might still contain nullptr)
+		else if (newCount < m_count)
+		{
+			Element *elementsOldEnd = m_elements + m_count;
+			m_count = newCount;
+			destruct(m_elements + newCount, elementsOldEnd);
+		}
+	}
 
-	// TODO: resize
+	// TODO: exceptions?
 
-	// TODO: element access
+	/// Gets an element by position, access violation on failure.
+	LEAN_INLINE reference at(size_type pos) { return m_elements[pos]; };
+	/// Gets an element by position, access violation on failure.
+	LEAN_INLINE const_reference at(size_type pos) const { return m_elements[pos]; };
+	/// Gets the first element in the vector, access violation on failure.
+	LEAN_INLINE reference front(void) { return *m_elements; };
+	/// Gets the first element in the vector, access violation on failure.
+	LEAN_INLINE const_reference front(void) const { return *m_elements; };
+	/// Gets the last element in the vector, access violation on failure.
+	LEAN_INLINE reference back(void) { return m_elements[m_count - 1]; };
+	/// Gets the last element in the vector, access violation on failure.
+	LEAN_INLINE const_reference back(void) const { return m_elements[m_count - 1]; };
+
+	/// Gets an element by position, access violation on failure.
+	LEAN_INLINE reference operator [](size_type pos) { return m_elements[pos]; };
+	/// Gets an element by position, access violation on failure.
+	LEAN_INLINE const_reference operator [](size_type pos) const { return m_elements[pos]; };
+
+	// TODO: check compilation behavior for repeated end() calls
+
+	/// Returns an iterator to the first element contained by this vector.
+	LEAN_INLINE iterator begin(void) { return m_elements; };
+	/// Returns a constant iterator to the first element contained by this vector.
+	LEAN_INLINE const_iterator begin(void) const { return m_elements; };
+	/// Returns an iterator beyond the last element contained by this vector.
+	LEAN_INLINE iterator end(void) { return m_elements + m_count; };
+	/// Returns a constant iterator beyond the last element contained by this vector.
+	LEAN_INLINE const_iterator end(void) const { return m_elements + m_count; };
+
+	/// Gets a copy of the allocator used by this vector.
+	LEAN_INLINE allocator_type get_allocator() const { return m_allocator; };
+
+	/// Returns the number of elements contained by this vector.
+	LEAN_INLINE size_type size(void) const { return m_count; };
+	/// Returns the number of elements this vector could contain without reallocation.
+	LEAN_INLINE size_type capacity(void) const { return m_capacity; };
 
 	/// Computes a new capacity based on the given number of elements to be stored.
 	LEAN_NOINLINE size_t capacity_hint(size_t count) const
@@ -220,6 +311,8 @@ public:
 	{
 		return s_max_size;
 	}
+
+	// TODO: swap?
 };
 
 } // namespace

af857de304efc38dc27defc736162ecaf6c6a7e8
opensource <opensource@alphanew.net>
2011-03-31 08:43:38 +0000

    - disabled operator new[] for aligned objects

--- memory_test/source/aligned_tests.cpp
+++ memory_test/source/aligned_tests.cpp
@@ -14,14 +14,18 @@ BOOST_AUTO_TEST_SUITE( aligned )
 
 BOOST_AUTO_TEST_CASE( stack_aligned_array )
 {
-	aligned_object a[16];
+	aligned_object a[7];
 	BOOST_CHECK( reinterpret_cast<uintptr_t>(a) % 16 == 0 );
 }
 
 BOOST_AUTO_TEST_CASE( aligned_array )
 {
-	aligned_object *a = new aligned_object[16];
+	aligned_object *a = new aligned_object[4];
+	aligned_object *b = new aligned_object[5];
 	BOOST_CHECK( reinterpret_cast<uintptr_t>(a) % 16 == 0 );
+	BOOST_CHECK( reinterpret_cast<uintptr_t>(b) % 16 == 0 );
+	delete[] a;
+	delete[] b;
 }
 
 BOOST_AUTO_TEST_SUITE_END()

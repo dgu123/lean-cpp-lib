77e7f05c458cb4dd50fc3bfdef9406113dbe77cf
opensource@alphanew.net <opensource@alphanew.net@3a0cca5d-a9e9-79d7-8999-1955a74f608b>
2011-04-08 16:14:24 +0000

    - cleaning up after string experiments

--- trunk/lean/header/lean/strings/char_traits.h
+++ trunk/lean/header/lean/strings/char_traits.h
@@ -7,7 +7,6 @@
 
 #include "../lean.h"
 #include <cstring>
-#include <algorithm>
 
 namespace lean
 {
@@ -22,6 +21,75 @@ struct char_traits
 	typedef Char char_type;
 	/// Corresponding integer type.
 	typedef typename int_type<sign_class::no_sign, sizeof(char_type)>::type int_type;
+	/// Size type.
+	typedef size_t size_type;
+	
+	/// Checks if the given character is null.
+	static LEAN_INLINE bool null(const char_type &src)
+	{
+		return (src == static_cast<char_type>(0));
+	}
+
+	/// Checks if the given null-terminated range of characters is empty.
+	static LEAN_INLINE bool empty(const char_type *begin)
+	{
+		return null(*begin);
+	}
+
+	/// Gets the length of the given null-terminated range of characters.
+	static size_type length(const char_type *begin)
+	{
+		size_t length = 0;
+
+		while (!null(*(begin++)))
+			++length;
+
+		return length;
+	}
+
+	/// Gets the number of code points in the given null-terminated range of characters.
+	static LEAN_INLINE size_type count(const char_type *begin)
+	{
+		return length(begin);
+	}
+
+	/// Converts the given integer into a character.
+	static LEAN_INLINE char_type to_char_type(const int_type &integer)
+	{
+		return static_cast<char_type>(integer);
+	}
+	/// Converts the given character into an integer.
+	static LEAN_INLINE int_type to_int_type(const char_type &character)
+	{
+		return static_cast<int_type>(character);
+	}
+
+	/// Compares the given number of characters.
+	static LEAN_INLINE int *compare(const char_type *dest, const char_type *source, size_t count)
+	{
+		const char_type *destEnd = dest + count;
+
+		while (dest != destEnd)
+			if (*(dest++) != *(source++))
+				// Handle null (end of string) using unsigned comparison
+				return (static_cast<int_type>(*dest) < static_cast<int_type>(*source)) ? -1 : 1;
+			else if (null(*dest))
+				break;
+		
+		return 0;
+	}
+
+	/// Copies the given number of source characters to the given destination characters.
+	static LEAN_INLINE char_type *copy(char_type *dest, const char_type *source, size_t count)
+	{
+		::memcpy(dest, source, count * sizeof(char_type));
+	}
+
+	/// Moves the given number of source characters to the given destination characters (may be overlapping ranges).
+	static LEAN_INLINE char_type *move(char_type *dest, const char_type *source, size_t count)
+	{
+		::memmove(dest, source, count * sizeof(char_type));
+	}
 
 	/// Assigns the given source character to the given destination character.
 	static LEAN_INLINE void assign(char_type &dest, const char_type &src)
@@ -36,97 +104,12 @@ struct char_traits
 		while (dest != destEnd)
 			*(dest++) = src;
 	}
-
-	/// Compares the given number of source characters to the destination characters.
-	static LEAN_INLINE int *copy(const char_type *dest, const char_type *source, size_t count)
-	{
-		const char_type *destEnd = dest + count;
-
-		while (dest != destEnd)
-			if (*(dest++) != *(source++))
-				return (*dest < *source) ? -1 : 1;
-			else
-
-
-		return std::lexicographical_compare(dest, source, source + count) ? -1;
-
-		return ::memcmp(dest, source, count * sizeof(char_type));
-	}
-
-	/// Copies the given number of source characters to the given destination characters.
-	static LEAN_INLINE char_type *copy(char_type *dest, const char_type *source, size_t count)
-	{
-		::memcpy(dest, source, count * sizeof(char_type));
-	}
-
-	/// Moves the given number of source characters to the given destination characters (may be overlapping ranges).
-	static LEAN_INLINE char_type *move(char_type *dest, const char_type *source, size_t count)
-	{
-		::memmove(dest, source, count * sizeof(char_type));
-	}
- 
-
-);
- 
-
 };
 
-/// Character range class that may be constructed from arbitrary string classes.
-template < class Char = utf8_t, class Traits = std::char_traits<Char> >
-class char_range
-{
-private:
-	const Char *m_begin;
-	const Char *m_end;
-
-	/// Computes the length of the given C string in elements.
-	static size_t length(const Char *begin)
-	{
-		LEAN_ASSERT(begin);
-	}
-
-public:
-	/// Type of the characters referenced by this class.
-	typedef Char value_type;
-	
-	/// Type of the size returned by this range.
-	typedef size_t size_type;
-	/// Type of the difference between the addresses of two elements in this range.
-	typedef ptrdiff_t difference_type;
-
-	/// Type of pointers to the elements contained by this range.
-	typedef value_type* pointer;
-	/// Type of constant pointers to the elements contained by this range.
-	typedef const value_type* const_pointer;
-	/// Type of references to the elements contained by this range.
-	typedef value_type& reference;
-	/// Type of constant references to the elements contained by this range.
-	typedef const value_type& const_reference;
-
-	/// Type of iterators to the elements contained by this range.
-	typedef pointer iterator;
-	/// Type of constant iterators to the elements contained by this range.
-	typedef const_pointer const_iterator;
-
-	/// Unsigned integer literal that indicates an invalid position.
-	static const size_type npos = static_cast<size_type>(-1);
-
-	/// Constructs a character range from the given characters.
-	LEAN_INLINE char_range(const_pointer begin, const_pointer end)
-		: m_begin(begin),
-		m_end(end) { }
-	/// Constructs a character range from the given characters.
-	LEAN_INLINE char_range(const_pointer begin, const_pointer end)
-		: m_begin(begin),
-		m_end(end) { }
-}
-
 } // namespace
 
-using strings::char_range;
+using strings::char_traits;
 
 } // namespace
-
-#include "char_range.h"
 
 #endif

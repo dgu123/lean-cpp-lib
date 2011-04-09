b73b4443b715483752c8632a333a74c9ec012210
opensource <opensource@alphanew.net>
2011-04-09 01:36:38 +0000

    - introduced nullterminated_range full range class  -> O(1) length() & end(), reduced cost for count()  - introduced even more efficient conversions for full ranges  - enhanced string types by full-range types, e.g. utf8_ntr

--- io_log_test/source/nullterminated_tests.cpp
+++ io_log_test/source/nullterminated_tests.cpp
@@ -1,13 +1,20 @@
 #include "stdafx.h"
 #include <lean/strings/strings.h>
+#include <iostream>
 
 BOOST_AUTO_TEST_SUITE( nullterminated )
 
 BOOST_AUTO_TEST_CASE( casts )
 {
 	std::string test("hello world");
-	lean::utf8_nt range(test);
-	std::string test2 = range;
+	lean::utf8_nt half_range(test);
+	lean::utf8_ntr range(test);
+	range = "hello cstring";
+	half_range = "hello cstring";
+	range = half_range;
+	half_range = range;
+	std::cout << half_range.c_str()<< std::endl;
+	std::cout << static_cast<std::string>(range) << std::endl;
 }
 
 BOOST_AUTO_TEST_SUITE_END()

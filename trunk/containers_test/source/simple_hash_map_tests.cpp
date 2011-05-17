687c030fa9225c11323292664280f8ffd319e5c3
opensource@alphanew.net <opensource@alphanew.net@3a0cca5d-a9e9-79d7-8999-1955a74f608b>
2011-05-17 22:53:03 +0000

    - nasty VS 2010 compiler bug workaround

--- trunk/containers_test/source/simple_hash_map_tests.cpp
+++ trunk/containers_test/source/simple_hash_map_tests.cpp
@@ -3,42 +3,11 @@
 #include <random>
 #include <vector>
 
-template <class Type>
-struct some_source
-{
-	static const Type value;
-};
-
-template <class Type> const Type some_source<Type>::value = Type(
-		(std::numeric_limits<Type>::min() != Type())
-			? std::numeric_limits<Type>::min()
-			: std::numeric_limits<Type>::max()
-	);
-
-template <class Type>
-struct some_dest
-{
-	static const Type value;
-};
-
-template <class Type> const Type some_dest<Type>::value(some_source<Type>::value);
-
 BOOST_AUTO_TEST_SUITE( simple_hash_map )
 
 BOOST_AUTO_TEST_CASE( int_pod )
 {
-	int dest = some_dest<int>::value;
-	int source = some_source<int>::value;
-
 	const size_t count = 10000;
-
-	int test = (std::numeric_limits<int>::has_infinity)
-				? std::numeric_limits<int>::infinity()
-				: (std::numeric_limits<int>::min() != int())
-					? std::numeric_limits<int>::min()
-					: std::numeric_limits<int>::max();
-	int test2 = lean::containers::impl::default_numeric_keys<int>::invalid_key;
-	int test3 = lean::containers::default_keys<int>::invalid_key;
 
 	typedef lean::simple_hash_map<int, int> map_type;
 	map_type map;

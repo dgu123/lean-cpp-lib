00681119d9331c6e7f75551147919962035be94c
opensource@alphanew.net <opensource@alphanew.net@3a0cca5d-a9e9-79d7-8999-1955a74f608b>
2011-05-19 21:18:48 +0000

    - improved simple_hash_map clear, fixed operator[] and string key comparison  - introduced benchmark project

---
+++ trunk/benchmark/header/stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#define LEAN_FAST_STL
+
+#include <lean/depconfig.h>
+#include <lean/lean.h>
+
+#include <lean/time/highres_timer.h>
+
+#include <iostream>
+
+inline void print_results(const char *test, const char *name1, double time1, const char *name2, double time2)
+{
+	std::cout << test << ":" << std::endl
+		<< " -> " << name1 << ": " << time1 << "    (" << time2 / time1 << ")" << std::endl
+		<< " -> " << name2 << ": " << time2 << "    (" << time1 / time2 << ")" << std::endl
+		<< std::endl;
+}
---
+++ trunk/benchmark/source/benchmark.cpp
@@ -0,0 +1,14 @@
+// benchmark.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+void hash_map_benchmark();
+
+int main()
+{
+	hash_map_benchmark();
+
+	return 0;
+}
+

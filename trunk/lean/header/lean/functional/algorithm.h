35ecd38e0e13622854462d0e884c4e93a3d7b96d
opensource@alphanew.net <opensource@alphanew.net@3a0cca5d-a9e9-79d7-8999-1955a74f608b>
2011-12-04 12:04:30 +0000

    - more algorithms

--- trunk/lean/header/lean/functional/algorithm.h
+++ trunk/lean/header/lean/functional/algorithm.h
@@ -15,6 +15,78 @@ namespace lean
 namespace functional
 {
 
+/// Compares the elements in the given ranges.
+template <class Iterator1, class Iterator2>
+inline bool equal(Iterator1 begin1, Iterator1 end1, Iterator2 begin2, Iterator2 end2)
+{
+	Iterator1 it1 = begin1;
+	Iterator1 it2 = begin2;
+
+	bool ended1 = (it1 == end1);
+	bool ended2 = (it2 == end2);
+
+	while (!ended1 && !ended2)
+	{
+		if (!(*it1 == *it2))
+			return false;
+
+		ended1 = (++it1 == end1);
+		ended2 = (++it2 == end2);
+	}
+
+	return ended1 && ended2;
+}
+
+/// Compares the elements in the given ranges using the given predicate.
+template <class Iterator1, class Iterator2, class Pred>
+inline bool equal(Iterator1 begin1, Iterator1 end1, Iterator2 begin2, Iterator2 end2, Pred pred)
+{
+	Iterator1 it1 = begin1;
+	Iterator1 it2 = begin2;
+
+	bool ended1 = (it1 == end1);
+	bool ended2 = (it2 == end2);
+
+	while (!ended1 && !ended2)
+	{
+		if (!pred(*it1, *it2))
+			return false;
+
+		ended1 = (++it1 == end1);
+		ended2 = (++it2 == end2);
+	}
+
+	return ended1 && ended2;
+}
+
+/// Compares the elements in the given ranges.
+template <class Range1, class Range2>
+inline bool equal(const Range1 &range1, const Range2 &range2)
+{
+	return equal(range1.begin(), range1.end(), range2.begin(), range2.end());
+}
+
+/// Compares the elements in the given ranges using the given predicate.
+template <class Range1, class Range2, class Pred>
+inline bool equal(const Range1 &range1, const Range2 &range2, Pred pred)
+{
+	return equal(range1.begin(), range1.end(), range2.begin(), range2.end(), pred);
+}
+
+/// Compares the elements in the given ranges.
+template <class Range1, class Range2>
+inline bool lexicographical_compare(const Range1 &range1, const Range2 &range2)
+{
+	return std::lexicographical_compare(range1.begin(), range1.end(), range2.begin(), range2.end());
+}
+
+/// Compares the elements in the given ranges using the given predicate.
+template <class Range1, class Range2, class Pred>
+inline bool lexicographical_compare(const Range1 &range1, const Range2 &range2, Pred pred)
+{
+	return std::lexicographical_compare(range1.begin(), range1.end(), range2.begin(), range2.end(), pred);
+}
+
 /// Inserts the element pointed at by <code>last</code> into the given sorted range <code>[first, last)</code>.
 template <class Iterator>
 inline Iterator insert_last(Iterator first, Iterator last)
@@ -81,6 +153,30 @@ inline typename Vector::iterator push_sorted(Vector &vector, const Value &value,
 
 #endif
 
+/// Locates the position of the first occurence of the given element in the given sorted range.
+template <class Iterator, class Value>
+inline Iterator find_sorted(Iterator begin, Iterator end, const Value &value)
+{
+	Iterator element = std::lower_bound(begin, end, value);
+
+	if (element != end && !(*element == value))
+		element = end;
+
+	return element;
+}
+
+/// Locates the position of the first occurence of the given element in the given sorted range.
+template <class Iterator, class Value, class Ord, class Eq>
+inline Iterator find_sorted(Iterator begin, Iterator end, const Value &value, Ord order, Eq equal)
+{
+	Iterator element = std::lower_bound(begin, end, value, order);
+
+	if (element != end && !equal(*element, value))
+		element = end;
+
+	return element;
+}
+
 /// Removes the given element from the given vector.
 template <class Vector, class Value>
 inline void remove(Vector &vector, const Value &value)
@@ -100,6 +196,7 @@ inline void remove_ordered(Vector &vector, const Value &value)
 	while (it != vector.end())
 	{
 		if (*it == value)
+			// Not quite optimal, assuming that elements mostly occur only once
 			it = vector.erase(it);
 		else
 			++it;
@@ -108,8 +205,11 @@ inline void remove_ordered(Vector &vector, const Value &value)
 
 } // namespace
 
+using functional::equal;
+using functional::lexicographical_compare;
 using functional::insert_last;
 using functional::push_sorted;
+using functional::find_sorted;
 using functional::remove;
 using functional::remove_ordered;
 

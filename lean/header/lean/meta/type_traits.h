6c3c65598b96219de6a79c51e6a7cd1d45d274f8
opensource <opensource@alphanew.net>
2012-02-13 11:27:02 +0000

    - introduced chunk_heap swap function  - introduced is_trivial type_trait family  - fixed dynamic_array template base class name lookup  - introduced numeric xml attribute helpers

--- lean/header/lean/meta/type_traits.h
+++ lean/header/lean/meta/type_traits.h
@@ -86,4 +86,55 @@ using meta::is_derived;
 
 } // namespace
 
+#ifndef _MSC_VER
+#include <type_traits>
+#endif
+
+namespace lean
+{
+namespace meta
+{
+
+#ifdef _MSC_VER
+
+/// Checks if the given type is trivial.
+template <class Type>
+struct is_trivial
+{
+	/// True, if the given type is trivial.
+	static const bool value = __has_trivial_constructor(Type) && __has_trivial_copy(Type) && __has_trivial_assign(Type) && __has_trivial_destructor(Type) || __is_pod(Type);
+};
+
+/// Checks if the given type is trivially copyable.
+template <class Type>
+struct is_trivially_copyable
+{
+	/// True, if the given type is trivial.
+	static const bool value = __has_trivial_copy(Type) && __has_trivial_assign(Type) && __has_trivial_destructor(Type) || __is_pod(Type);
+};
+
+/// Checks if the given type is trivially destructible.
+template <class Type>
+struct is_trivially_destructible
+{
+	/// True, if the given type is trivial.
+	static const bool value = __has_trivial_destructor(Type) || __is_pod(Type);
+};
+
+#else
+
+using std::is_trivial;
+using std::is_trivially_copyable;
+using std::is_trivially_destructible;
+
+#endif
+
+} // namespace
+
+using meta::is_trivial;
+using meta::is_trivially_copyable;
+using meta::is_trivially_destructible;
+
+} // namespace
+
 #endif

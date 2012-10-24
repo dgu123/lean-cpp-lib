d717c9a413ee6ca8d2561dda304d56bf5a4109c6
opensource@alphanew.net <opensource@alphanew.net@3a0cca5d-a9e9-79d7-8999-1955a74f608b>
2012-10-24 18:23:03 +0000

    - simple_vector erase

--- trunk/lean/header/lean/containers/construction.h
+++ trunk/lean/header/lean/containers/construction.h
@@ -16,6 +16,11 @@ namespace containers
 struct nontrivial_construction_t { };
 struct trivial_construction_t { };
 
+template <class Tag>
+struct is_trivial_construction { static const bool value = false; };
+template <>
+struct is_trivial_construction<trivial_construction_t> { static const bool value = true; };
+
 /// Destructs the elements in the given range.
 template <class Element, class Allocator>
 LEAN_INLINE void destruct(Element *destr, Allocator &allocator, nontrivial_construction_t = nontrivial_construction_t())
@@ -71,7 +76,7 @@ LEAN_INLINE void copy_construct(Element *dest, const Element &source, Allocator 
 }
 /// Copies elements from the given source range to the given destination.
 template <class Iterator, class Element, class Allocator>
-inline void copy_construct(Iterator source, Iterator sourceEnd, Element *dest, Allocator &allocator, nontrivial_construction_t = nontrivial_construction_t())
+inline Element* copy_construct(Iterator source, Iterator sourceEnd, Element *dest, Allocator &allocator, nontrivial_construction_t = nontrivial_construction_t())
 {
 	Element *destr = dest;
 
@@ -85,11 +90,15 @@ inline void copy_construct(Iterator source, Iterator sourceEnd, Element *dest, A
 		destruct(destr, dest, allocator);
 		throw;
 	}
+
+	return dest;
 }
 template <class Iterator, class Element, class Allocator>
-LEAN_INLINE void copy_construct(Iterator source, Iterator sourceEnd, Element *dest, Allocator &allocator, trivial_construction_t)
+LEAN_INLINE Element* copy_construct(Iterator source, Iterator sourceEnd, Element *dest, Allocator &allocator, trivial_construction_t)
 {
-	memcpy(dest, addressof(*source), (sourceEnd - source) * sizeof(Element));
+	size_t count = sourceEnd - source;
+	memcpy(dest, addressof(*source), count * sizeof(Element));
+	return dest + count;
 }
 
 /// Moves the given source element to the given destination.
@@ -105,7 +114,7 @@ LEAN_INLINE void move_construct(Element *dest, Element &source, Allocator &alloc
 }
 /// Moves elements from the given source range to the given destination.
 template <class Iterator, class Element, class Allocator>
-inline void move_construct(Iterator source, Iterator sourceEnd, Element *dest, Allocator &allocator, nontrivial_construction_t = nontrivial_construction_t())
+inline Element* move_construct(Iterator source, Iterator sourceEnd, Element *dest, Allocator &allocator, nontrivial_construction_t = nontrivial_construction_t())
 {
 	Element *destr = dest;
 
@@ -119,11 +128,13 @@ inline void move_construct(Iterator source, Iterator sourceEnd, Element *dest, A
 		destruct(destr, dest, allocator);
 		throw;
 	}
+
+	return dest;
 }
 template <class Iterator, class Element, class Allocator>
-LEAN_INLINE void move_construct(Iterator source, Iterator sourceEnd, Element *dest, Allocator &allocator, trivial_construction_t)
+LEAN_INLINE Element* move_construct(Iterator source, Iterator sourceEnd, Element *dest, Allocator &allocator, trivial_construction_t)
 {
-	copy_construct(source, sourceEnd, dest, allocator, trivial_construction_t());
+	return copy_construct(source, sourceEnd, dest, allocator, trivial_construction_t());
 }
 
 /// Moves the given source element to the given destination.
@@ -139,28 +150,83 @@ LEAN_INLINE void move(Element *dest, Element &source, trivial_construction_t)
 }
 /// Moves elements from the given source range to the given destination.
 template <class Iterator, class Element>
-inline void move(Iterator source, Iterator sourceEnd, Element *dest, nontrivial_construction_t = nontrivial_construction_t())
+inline Element* move(Iterator source, Iterator sourceEnd, Element *dest, nontrivial_construction_t = nontrivial_construction_t())
 {
 	for (; source != sourceEnd; ++dest, ++source)
 		move(dest, *source);
+
+	return dest;
 }
 template <class Iterator, class Element>
-LEAN_INLINE void move(Iterator source, Iterator sourceEnd, Element *dest, trivial_construction_t)
+LEAN_INLINE Element* move(Iterator source, Iterator sourceEnd, Element *dest, trivial_construction_t)
 {
-	memcpy(dest, addressof(*source), (sourceEnd - source) * sizeof(Element));
+	size_t count = sourceEnd - source;
+	memmove(dest, addressof(*source), count * sizeof(Element));
+	return dest + count;
 }
 /// Moves elements from the given source range to the given destination.
 template <class Iterator, class Element>
-inline void move_backwards(Iterator source, Iterator sourceEnd, Element *destEnd, nontrivial_construction_t = nontrivial_construction_t())
+inline Element* move_backwards(Iterator source, Iterator sourceEnd, Element *dest, nontrivial_construction_t = nontrivial_construction_t())
 {
-	while (source != sourceEnd)
-		move(--destEnd, *--sourceEnd);
+	Element *destEnd = dest + (sourceEnd - source);
+
+	for (dest = destEnd; source != sourceEnd; )
+		move(--dest, *--sourceEnd);
+
+	return destEnd;
 }
 template <class Iterator, class Element>
-LEAN_INLINE void move_backwards(Iterator source, Iterator sourceEnd, Element *destEnd, trivial_construction_t)
+LEAN_INLINE Element* move_backwards(Iterator source, Iterator sourceEnd, Element *dest, trivial_construction_t)
 {
 	size_t count = (sourceEnd - source);
-	memmove(destEnd - count, addressof(*source), count * sizeof(Element));
+	memmove(dest, addressof(*source), count * sizeof(Element));
+	return dest + count;
+}
+
+/// Opens a gap of elements, returning the new end element.
+template <class Element, class Allocator, class DestructTag>
+LEAN_INLINE void open(Element *gap, Element *gapEnd, Element *&end, Allocator &allocator, trivial_construction_t moveTag, DestructTag destructTag)
+{
+	LEAN_ASSERT(gap <= gapEnd);
+	LEAN_ASSERT(gap <= end);
+
+	end = move_backwards(gap, end, gapEnd, moveTag);
+}
+/// Opens a gap of elements, returning the new end element via the given end reference.
+template <class Element, class Allocator, class DestructTag>
+inline void open(Element *gap, Element *gapEnd, Element *&end, Allocator &allocator, nontrivial_construction_t moveTag, DestructTag destructTag)
+{
+	LEAN_ASSERT(gap <= gapEnd);
+	LEAN_ASSERT(gap <= end);
+
+	size_t gapWidth = gapEnd - gap;
+	size_t oldCount = end - gap;
+	Element *oldEnd = end;
+	Element *newEnd = end + gapWidth;
+	
+	size_t constructCount = min(gapWidth, oldCount);
+	end = move_construct(oldEnd - constructCount, oldEnd, newEnd - constructCount, allocator, moveTag);
+	
+	move_backwards(gap, oldEnd - constructCount, gapEnd, moveTag);
+
+	destruct(gap, gap + constructCount, allocator, destructTag);
+}
+
+/// Closes a gap of elements, returning the new end element via the given end reference.
+template <class Element, class Allocator, class MoveTag, class DestructTag>
+inline void close(Element *gap, Element *gapEnd, Element *&end, Allocator &allocator, MoveTag moveTag, DestructTag destructTag)
+{
+	LEAN_ASSERT(gap <= gapEnd);
+	LEAN_ASSERT(gapEnd <= end);
+
+	if (is_trivial_construction<MoveTag>::value)
+		destruct(gap, gapEnd, allocator, destructTag);
+
+	Element *oldEnd = end;
+	end = move(gapEnd, oldEnd, gap, moveTag);
+	
+	if (!is_trivial_construction<MoveTag>::value)
+		destruct(end, oldEnd, allocator, destructTag);
 }
 
 } // namespace

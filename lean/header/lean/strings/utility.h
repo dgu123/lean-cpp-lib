36fa2c39cff7fcd67f3309e97ebbfc57987bc09d
opensource <opensource@alphanew.net>
2012-12-14 17:04:14 +0000

    - djb2 hash  - arbitrary int_to_char base  - got rid of range->half-range inheritance relationship  - range construction

--- lean/header/lean/strings/utility.h
+++ lean/header/lean/strings/utility.h
@@ -17,34 +17,40 @@ namespace strings
 	/// Copies at maximum the given number of characters from source to dest string, always incorporating a terminating null.
 	inline size_t strmcpy(char *dest, const char *source, size_t maxChars)
 	{
-		if (maxChars > 0)
-		{
-			size_t len = min(::strlen(source), maxChars - 1);
-			::memcpy(dest, source, len);
-			dest[len] = 0;
-			return len;
-		}
-		else
-			return 0;
+		LEAN_ASSERT(maxChars > 0);
+
+		size_t len = min(::strlen(source), maxChars - 1);
+		::memcpy(dest, source, len);
+		dest[len] = 0;
+		return len;
 	}
 
 	/// Copies at maximum the given number of characters from source to dest string, always incorporating a terminating null.
 	inline size_t wcsmcpy(wchar_t *dest, const wchar_t *source, size_t maxChars)
 	{
-		if (maxChars > 0)
-		{
-			size_t len = min(::wcslen(source), maxChars - 1);
-			::memcpy(dest, source, len * sizeof(wchar_t));
-			dest[len] = 0;
-			return len;
-		}
-		else
-			return 0;
+		LEAN_ASSERT(maxChars > 0);
+
+		size_t len = min(::wcslen(source), maxChars - 1);
+		::memcpy(dest, source, len * sizeof(wchar_t));
+		dest[len] = 0;
+		return len;
+	}
+
+	/// Copies at maximum the given number of characters from source to dest string, always incorporating a terminating null.
+	template <class Char, class Range>
+	inline Char* strcpy_range(Char *dest, const Range &source)
+	{
+		size_t len = source.end() - source.begin();
+		::memcpy(dest, static_cast<const Char*>(source.begin()), len * sizeof(Char));
+		dest[len] = 0;
+		return dest;
 	}
 
 } // namespace
 
 using strings::strmcpy;
+using strings::strcpy_range;
+
 using strings::wcsmcpy;
 
 } // namespace

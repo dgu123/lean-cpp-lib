341410dee7c74afa9a27f65598cde9cf9a6a27bf
opensource@alphanew.net <opensource@alphanew.net@3a0cca5d-a9e9-79d7-8999-1955a74f608b>
2013-01-07 11:04:32 +0000

    - base class behavior macros  - changed resource ref_counter visibility to public

--- trunk/lean/header/lean/smart/resource.h
+++ trunk/lean/header/lean/smart/resource.h
@@ -12,20 +12,22 @@
 /// @see lean::smart::resource
 /// @{
 
+/*
 /// Allows resource pointers access to a class' reference counter.
 #define LEAN_MAKE_RESOURCE \
 	template <class Resource, bool Critical> \
 	friend class lean::smart::resource_ptr; \
 	template <class Resource> \
 	friend class lean::smart::weak_resource_ptr;
+*/
 
 /// Overrides the @code resource_interface@endcode methods required for a resource interface to treated like a resource.
 /// WARNING: Changes class visibility.
-#define LEAN_RENEW_RESOURCE LEAN_MAKE_RESOURCE \
-	protected: \
-		const lean_resource_base::ref_counter_type& ref_counter() const { return lean_resource_base::ref_counter(); } \
+#define LEAN_RENEW_RESOURCE \
 	public: \
-		lean_resource_base::ref_counter_type::counter_type ref_count() const { return lean_resource_base::ref_count(); }
+		const lean_resource_base::ref_counter_type& ref_counter() const { return lean_resource_base::ref_counter(); } \
+		lean_resource_base::ref_counter_type::counter_type ref_count() const { return lean_resource_base::ref_count(); } \
+	private:
 
 /// @}
 
@@ -44,14 +46,16 @@ class weak_resource_ptr;
 template < class Counter = long, class Allocator = std::allocator<Counter>, bool Lazy = false >
 class resource
 {
-	LEAN_MAKE_RESOURCE
+//	LEAN_MAKE_RESOURCE
 
-protected:
+public:
 	/// Resource base type.
 	typedef resource lean_resource_base;
-
 	/// Reference counter type.
 	typedef ref_counter<Counter, Allocator> ref_counter_type;
+
+protected:
+	/// Reference counter type.
 	mutable ref_counter_type m_refCounter;
 
 private:
@@ -70,7 +74,7 @@ protected:
 	resource(const typename ref_counter_type::allocator_type& allocator)
 		: m_refCounter(allocator) { }
 	/// Copy constructor.
-	LEAN_INLINE resource(const resource& right) noexcept { }
+	LEAN_INLINE resource(const resource& right) noexcept { } // TODO: Copy allocator?
 	/// Assignment operator.
 	LEAN_INLINE resource& operator =(const resource& right) noexcept { return *this; }
 #ifndef LEAN_OPTIMIZE_DEFAULT_DESTRUCTOR
@@ -78,6 +82,7 @@ protected:
 	LEAN_INLINE ~resource() noexcept { }
 #endif
 
+public:
 	/// Returns the reference counter of this resource.
 	const ref_counter_type& ref_counter() const
 	{
@@ -88,26 +93,21 @@ protected:
 		}
 		return m_refCounter;
 	}
-
-public:
 	/// Gets the reference count.
 	typename ref_counter_type::counter_type ref_count() const { return m_refCounter.count(); }
 };
 
 /// Interface that may be used to tag a specific class as a resource.
 template < class Counter = long, class Allocator = std::allocator<Counter> >
-class resource_interface
+class LEAN_INTERFACE resource_interface
 {
 	LEAN_SHARED_INTERFACE_BEHAVIOR(resource_interface)
-	LEAN_MAKE_RESOURCE
 
-protected:
+public:
 	/// Reference counter type.
 	typedef ref_counter<Counter, Allocator> ref_counter_type;
 	/// Returns the reference counter of this resource.
 	virtual const ref_counter_type& ref_counter() const = 0;
-
-public:
 	/// Gets the reference count.
 	virtual typename ref_counter_type::counter_type ref_count() const = 0;
 };
